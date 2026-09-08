c37d177cc25cf8da6dc23f9c919c738ecb1feb05
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-09-08 07:00:52 +0600

    caching is added

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,5 +1,7 @@
 export const getUser = async() =>{
-    const res = await fetch("https://bloodlink-serverside.vercel.app/users");
+    const res = await fetch("https://bloodlink-serverside.vercel.app/users", {
+        cache: "no-store",
+    });
     const data = await res.json();
     return data;
 }
