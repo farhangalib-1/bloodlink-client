12f2e576fb6cef2a6ab36914ec054438dd48589f
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-09-16 17:58:13 +0600

    profile image loading is updated

--- src/components/IndividualUserInfo.jsx
+++ src/components/IndividualUserInfo.jsx
@@ -21,7 +21,7 @@ const IndividualUserInfo = ({ userInfo }) => {
      <div >
         <div className="p-4 m-7 border-2 border-gray-50 rounded-2xl shadow-lg shadow-gray-50 grid md:grid-cols-3 grid-cols-1  gap-5 ">
             <div className="flex flex-col items-center gap-2">
-            <Image src={userInfo.image} alt={userInfo.name} width={200} height={200} className="rounded-2xl" />
+            <Image src={userInfo.image} alt={userInfo.name} width={200} height={200}  priority quality={75} sizes="200px" className="rounded-2xl" />
             <p className="text-green-600 font-semibold text-xs flex items-center"> <GoDotFill/> Availabe to Donate</p>
             </div>
             <div className="space-y-1">
